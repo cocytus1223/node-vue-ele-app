23c0374fb6b02eacc76d250b8a40541ba6c788e8
章志成 <33870333+whitehairpin1223@users.noreply.github.com>
2018-11-08 01:09:26 +0800

    client init

---
+++ client/src/views/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="index">
+    初始化页面
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'index',
+    comments: {}
+  }
+</script>
+
+<style scoped>
+
+</style>
